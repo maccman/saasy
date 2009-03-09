d1b7de71102111009513b49583e229d530ccaa8e
Alex MacCaw <maccman@gmail.com>
2009-03-09 17:24:26 +0000

    Disable submit button on slow forms

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,7 +4,9 @@
 Element.observe(window, 'load', function(){
   // This is for slow forms (like the signup one),
   // so the user doesn't click 'Submit' twice.
-  $$('form.enhanced').each(function(frm){
+  $$('form.slow').each(function(frm){
+    var sub = frm.down("input[type='submit']");
+    if(sub) sub.disabled = '';
     $(frm).observe('submit', function(e){
       var sub = frm.down("input[type='submit']");
       if(sub){
